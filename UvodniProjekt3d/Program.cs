29ee79acf46092665990f0fd85f12313c92b283f
Lovro Šverko <lovro.sverko@gmail.com>
2021-11-16 11:17:19 +0100

    Add files via upload

---
+++ UvodniProjekt3d/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UvodniProjekt3d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // varijable
+            int ocjena, zbrojOcjena = 0 ;
+            double prosjek = 0, brojOcjena = 0;
+
+            // zaglavlje aplikacije
+            Console.WriteLine("##############################");
+            Console.WriteLine("#   Izračunavanje prosjeka   #");
+            Console.WriteLine("##############################");
+            Console.WriteLine("Za kraj unesi nulu.");
+
+            do
+            {
+                // unos ocjene
+                Console.WriteLine("Unesi ocjenu: ");
+                ocjena = Convert.ToInt32(Console.ReadLine());
+
+                // provjera ispravnosti unosa ocjena
+                if (ocjena <= 5 && ocjena > 1)
+                {
+                    // dodavanje ocjene u zbrojOcjena
+                    zbrojOcjena += ocjena;
+
+                    // brojanje predmeta
+                    brojOcjena++;
+
+                    //izračunavanje prosjeka
+                    prosjek = zbrojOcjena / brojOcjena;
+                }
+                else if (ocjena == 1)
+                {
+                    prosjek = 1;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Molim upišite ispravnu ocjenu.");
+                }
+            }
+            while (ocjena != 0);
+
+            // ispis prosjeka
+            
+            Console.WriteLine("Prosjek ocjena je: " + prosjek);
+
+            // kraj aplikacije
+            Console.ReadKey();
+        }
+    }
+}
